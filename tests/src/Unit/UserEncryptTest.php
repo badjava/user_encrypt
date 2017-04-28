3f713667175add9b69b09e5a15936430bead20bc
Chad DeGroot <badjava@gmail.com>
2017-04-28 09:38:48 -0700

    Adding tests.

---
+++ tests/src/Unit/UserEncryptTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\user_encrypt\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests for User Encrypt.
+ *
+ * @group user_encrypt
+ */
+class UserEncryptTest extends UnitTestCase {
+
+  /**
+   * Basic test to get success result.
+   */
+  public function testUserEncrypt() {
+    $this->assertTrue(TRUE);
+  }
+
+}
